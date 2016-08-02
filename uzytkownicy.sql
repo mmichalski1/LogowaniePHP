7fc3249f018a72a198040513e2465deb0e331988
Damian Kobyliński <damiankob12@gmail.com>
2016-08-02 12:37:28 +0200

    Add files via upload

---
+++ uzytkownicy.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 02 Sie 2016, 12:36
+-- Wersja serwera: 10.1.13-MariaDB
+-- Wersja PHP: 7.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `logowanie01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `ID` int(11) NOT NULL,
+  `user` text COLLATE utf8_polish_ci NOT NULL,
+  `password` text COLLATE utf8_polish_ci NOT NULL,
+  `nickname` text COLLATE utf8_polish_ci NOT NULL,
+  `e-mail` text COLLATE utf8_polish_ci NOT NULL,
+  `fonty` int(11) NOT NULL,
+  `diamenty` int(11) NOT NULL,
+  `premium` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`ID`, `user`, `password`, `nickname`, `e-mail`, `fonty`, `diamenty`, `premium`) VALUES
+(1, 'damian', 'qazwsx', 'Zero', 'lubieplacki@o2.pl', 23450, 32, 2);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
